0ef5f78308203f650e236267a0bbbc8dbd4c1bdd
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2025-02-12 16:48:55 +0300

    WIP

---
+++ app.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const path = require('node:path');
+const { Pool } = require('pg');
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const pool = new Pool({
+    host: 'localhost', // or wherever the db is hosted
+    user: 'kevin',
+    database: 'top_users',
+    password: process.env.DB_PW,
+    port: 5432, // The default port
+});
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const { rows } = await pool.query(
+                'SELECT FROM users WHERE username = $1',
+                [username],
+            );
+            const user = rows[0];
+
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username' });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    }),
+);
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/sign-up-form', (req, res) => res.render('sign-up-form'));
+app.post('/sign-up-form', async (req, res, next) => {
+    try {
+        await pool.query(
+            'INSERT INTO users (username, password) VALUES ($1, $2)',
+            [req.body.username, req.body.password],
+        );
+        res.redirect('/');
+    } catch (err) {
+        return next(err);
+    }
+});
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
